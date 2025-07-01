4603e5e2d964c6af4ed22dc8821606fd269eeee7
Gian Condori Luna <francoguns16@gmail.com>
2025-07-01 02:40:01 -0300

    Finalizado proyecto

--- frontend/lib/services/device_id_service.dart
+++ frontend/lib/services/device_id_service.dart
@@ -1,29 +1,22 @@
-import 'dart:io';
-import 'package:device_info_plus/device_info_plus.dart';
+// device_id_service.dart
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:uuid/uuid.dart';
 
-Future<String> getDeviceId() async {
-  final deviceInfo = DeviceInfoPlugin();
-
-  if (Platform.isAndroid) {
-    final androidInfo = await deviceInfo.androidInfo;
-    return androidInfo.id ?? 'unknown_android';
-  } else if (Platform.isIOS) {
-    final iosInfo = await deviceInfo.iosInfo;
-    return iosInfo.identifierForVendor ?? 'unknown_ios';
-  } else {
-    return 'unknown_device';
-  }
-}
-
+/// Devuelve un ID persistente único por instalación del app.
+/// Si no existe, genera un UUID nuevo y lo guarda localmente.
 Future<String> getOrCreateUserId() async {
   final prefs = await SharedPreferences.getInstance();
+  
+  await prefs.remove('userId');
+
   String? userId = prefs.getString('userId');
 
-  if (userId == null) {
-    userId = await getDeviceId();
+  if (userId == null || userId == 'UP1A.231005.007' || userId == 'TE1A.220922.010' || userId.startsWith('unknown')) {
+    userId = const Uuid().v4(); // ← genera un UUID único
     await prefs.setString('userId', userId);
   }
-
+  print("****************   userId  *********************");
+  print(userId);
+  print("************************************************");
   return userId;
 }
